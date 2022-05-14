d74400f719b707ed76a6584afd6b9a2a8630402e
Black Cat <iblackcatw@gmail.com>
2022-05-14 11:27:05 +0400

    Add icosahedron and decosahedron

---
+++ src/nodes/surfaces/dodecahedron.zig
@@ -0,0 +1,51 @@
+const util = @import("../node_utils.zig");
+
+const info = util.nsdf.Dodecahedron.info;
+
+pub const Dodecahedron: util.NodeType = .{
+    .name = info.name,
+    .function_definition = info.function_definition,
+
+    .properties = properties[0..],
+
+    .init_data_fn = initData,
+    .enter_command_fn = info.enter_command_fn,
+    .exit_command_fn = info.exit_command_fn,
+    .append_mat_check_fn = info.append_mat_check_fn,
+    .appendGizmosFn = appendGizmos,
+};
+
+const Data = util.nsdf.Dodecahedron.Data;
+
+const properties = [_]util.NodeProperty{
+    .{
+        .drawFn = util.prop.drawFloatProperty,
+        .offset = @offsetOf(Data, "radius"),
+        .name = "Radius",
+    },
+    .{
+        .drawFn = util.prop.drawFloatProperty,
+        .offset = @offsetOf(Data, "corner_radius"),
+        .name = "Corner Radius",
+    },
+    .{
+        .drawFn = util.prop.drawMaterialProperty,
+        .offset = @offsetOf(Data, "mat"),
+        .name = "Material",
+    },
+};
+
+fn initData(buffer: *[]u8) void {
+    const data: *Data = util.nyan.app.allocator.create(Data) catch unreachable;
+
+    data.radius = 1.0;
+    data.corner_radius = 0.1;
+    data.mat = 0;
+
+    buffer.* = util.std.mem.asBytes(data);
+}
+
+fn appendGizmos(buffer: *[]u8, gizmos_storage: *util.GizmoStorage) void {
+    _ = buffer;
+    _ = gizmos_storage;
+}
---
+++ src/nodes/surfaces/icosahedron_edges.zig
@@ -0,0 +1,51 @@
+const util = @import("../node_utils.zig");
+
+const info = util.nsdf.IcosahedronEdges.info;
+
+pub const IcosahedronEdges: util.NodeType = .{
+    .name = info.name,
+    .function_definition = info.function_definition,
+
+    .properties = properties[0..],
+
+    .init_data_fn = initData,
+    .enter_command_fn = info.enter_command_fn,
+    .exit_command_fn = info.exit_command_fn,
+    .append_mat_check_fn = info.append_mat_check_fn,
+    .appendGizmosFn = appendGizmos,
+};
+
+const Data = util.nsdf.IcosahedronEdges.Data;
+
+const properties = [_]util.NodeProperty{
+    .{
+        .drawFn = util.prop.drawFloatProperty,
+        .offset = @offsetOf(Data, "radius"),
+        .name = "Radius",
+    },
+    .{
+        .drawFn = util.prop.drawFloatProperty,
+        .offset = @offsetOf(Data, "edge_radius"),
+        .name = "Edge Radius",
+    },
+    .{
+        .drawFn = util.prop.drawMaterialProperty,
+        .offset = @offsetOf(Data, "mat"),
+        .name = "Material",
+    },
+};
+
+fn initData(buffer: *[]u8) void {
+    const data: *Data = util.nyan.app.allocator.create(Data) catch unreachable;
+
+    data.radius = 1.0;
+    data.edge_radius = 0.1;
+    data.mat = 0;
+
+    buffer.* = util.std.mem.asBytes(data);
+}
+
+fn appendGizmos(buffer: *[]u8, gizmos_storage: *util.GizmoStorage) void {
+    _ = buffer;
+    _ = gizmos_storage;
+}
