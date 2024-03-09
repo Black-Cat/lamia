3ee5d18265f5f83a5e5b81e180c2b434392a3ad3
Black Cat <iblackcatw@gmail.com>
2024-03-09 12:19:27 +0000

    Parse STEP File Name field

--- src/ui/import_step.zig
+++ src/ui/import_step.zig
@@ -23,12 +23,58 @@ pub const StepData = struct {
         implementation_level: [2]u3,
     };
 
+    const DateTime = struct {
+        year: u16 = 0,
+        month: u4 = 0,
+        day: u5 = 0,
+        hours: u5 = 0,
+        minutes: u6 = 0,
+        seconds: u6 = 0,
+        time_zone: i5 = 0,
+
+        fn parse(str: []const u8) !DateTime {
+            var dt: DateTime = .{};
+            dt.year = try std.fmt.parseInt(@TypeOf(dt.year), str[0..4], 10);
+            dt.month = try std.fmt.parseInt(@TypeOf(dt.month), str[5..7], 10);
+            dt.day = try std.fmt.parseInt(@TypeOf(dt.day), str[8..10], 10);
+            if (str.len <= 10 or str[10] != 'T')
+                return dt;
+
+            dt.hours = try std.fmt.parseInt(@TypeOf(dt.hours), str[11..13], 10);
+            dt.minutes = try std.fmt.parseInt(@TypeOf(dt.minutes), str[14..16], 10);
+            dt.seconds = try std.fmt.parseInt(@TypeOf(dt.seconds), str[17..19], 10);
+            if (str.len <= 20)
+                return dt;
+
+            dt.time_zone = try std.fmt.parseInt(@TypeOf(dt.time_zone), str[19..22], 10);
+            return dt;
+        }
+    };
+
+    const FileName = struct {
+        name: []const u8,
+        time_stamp: DateTime,
+        author: []const u8,
+        organization: []const u8,
+        preprocessor_version: []const u8,
+        originating_system: []const u8,
+        authorization: []const u8,
+    };
+
     allocator: std.mem.Allocator,
     iso: [2]u16,
     file_description: FileDescription,
+    file_name: FileName,
 
     fn destroy(self: *StepData) void {
         self.allocator.free(self.file_description.description);
+
+        self.allocator.free(self.file_name.name);
+        self.allocator.free(self.file_name.author);
+        self.allocator.free(self.file_name.organization);
+        self.allocator.free(self.file_name.preprocessor_version);
+        self.allocator.free(self.file_name.originating_system);
+        self.allocator.free(self.file_name.authorization);
     }
 };
 
@@ -61,6 +107,44 @@ fn parseFileDescription(step_data: *StepData, content: []const u8) !void {
     const first_imp_pos: usize = (std.mem.indexOfScalarPos(u8, content, desc_end + 2, '\'') orelse return error.OutOfBounds) + 1;
     step_data.file_description.implementation_level[0] = @intCast(content[first_imp_pos] - '0');
     step_data.file_description.implementation_level[1] = @intCast(content[first_imp_pos + 2] - '0');
+}
+
+fn parseFileName(step_data: *StepData, content: []const u8) !void {
+    var it = std.mem.splitScalar(u8, content, '\'');
+
+    _ = it.next() orelse return error.OutOfBounds;
+    var name: []const u8 = it.next() orelse return error.OutOfBounds;
+    var unescaped_name = std.ArrayList(u8).init(step_data.allocator);
+    var ind: usize = 0;
+    while (ind < name.len) {
+        if (name[ind] == '\\') {
+            ind += 1;
+            if (ind >= name.len)
+                break;
+        }
+
+        try unescaped_name.append(name[ind]);
+        ind += 1;
+    }
+    step_data.file_name.name = try unescaped_name.toOwnedSlice();
+
+    _ = it.next() orelse return error.OutOfBounds;
+    var time_stamp: []const u8 = it.next() orelse return error.OutOfBounds;
+    step_data.file_name.time_stamp = try StepData.DateTime.parse(time_stamp);
+
+    var fields: [5]*[]const u8 = [_]*[]const u8{
+        &step_data.file_name.author,
+        &step_data.file_name.organization,
+        &step_data.file_name.preprocessor_version,
+        &step_data.file_name.originating_system,
+        &step_data.file_name.authorization,
+    };
+
+    for (fields) |f| {
+        _ = it.next() orelse return error.OutOfBounds;
+        var field_val: []const u8 = it.next() orelse return error.OutOfBounds;
+        f.* = try step_data.allocator.dupe(u8, field_val);
+    }
 }
 
 fn parseHeader(step_data: *StepData, reader: anytype) !void {
@@ -105,7 +189,7 @@ fn parseHeader(step_data: *StepData, reader: anytype) !void {
         const command: StepHeaderCommand = std.meta.stringToEnum(StepHeaderCommand, hl[0..command_end_pos]) orelse continue;
         switch (command) {
             .FILE_DESCRIPTION => try parseFileDescription(step_data, hl[command_end_pos..]),
-            .FILE_NAME => {},
+            .FILE_NAME => try parseFileName(step_data, hl[command_end_pos..]),
             .FILE_SCHEMA => {},
         }
     }
